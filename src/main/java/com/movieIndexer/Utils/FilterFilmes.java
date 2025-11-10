ac582e45495f5a6fe211134353953b1234e9de88
Flavia Cristina Fagundes <flaviacrisfagundes@gmail.com>
2025-11-10 08:39:23 -0300

    Classe para filtrar as categorias criada

---
+++ src/main/java/com/movieIndexer/Utils/FilterFilmes.java
@@ -0,0 +1,25 @@
+package com.movieIndexer.Utils;
+
+import com.movieIndexer.Model.Filme;
+import java.util.Comparator;
+import java.util.Map;
+
+public class FilterFilmes {
+
+    public static Comparator<Map.Entry<Integer, Filme>> porTitulo() {
+        return Comparator.comparing(entry -> entry.getValue().getTitulo(), String.CASE_INSENSITIVE_ORDER);
+    }
+
+    public static Comparator<Map.Entry<Integer, Filme>> porDiretor() {
+        return Comparator.comparing(entry -> entry.getValue().getDiretor(), String.CASE_INSENSITIVE_ORDER);
+    }
+
+    public static Comparator<Map.Entry<Integer, Filme>> porAnoDeLancamento() {
+        return Comparator.comparingInt(entry -> entry.getValue().getAnoLancamento());
+    }
+
+    public static Comparator<Map.Entry<Integer, Filme>> porDuracao() {
+        return Comparator.comparing(entry -> entry.getValue().getDuracao());
+    }
+
+}
