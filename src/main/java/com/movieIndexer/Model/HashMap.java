9df0b17ec4465bea488c18abbe67930b3f872951
Lucas Aioria Serpa <136469417+LucasAioriaSerpa@users.noreply.github.com>
2025-11-10 07:27:26 -0300

    Update HashMap.java

--- src/main/java/com/movieIndexer/Model/HashMap.java
+++ src/main/java/com/movieIndexer/Model/HashMap.java
@@ -1,5 +1,28 @@
 package com.movieIndexer.Model;
 
+import java.util.LinkedList;
+
 public class HashMap {
+    private LinkedList<String>[] keys;
+    private int size;
+    public HashMap(int size) {
+        this.size = size;
+        keys = new LinkedList[size];
+        for (int i = 0; i < size; i++) { keys[i] = new LinkedList<>(); }
+    }
+
+    //? GETTERS
+    public LinkedList<String>[] getKeys() { return keys; }
+    public int getSize() { return size; }
+
+    //? SETTERS
+    public void setKeys(LinkedList<String>[] keys) { this.keys = keys; }
+    public void setSize(int size) { this.size = size; }
+
+    //? =====================================================================================
+
+    private int hashCode(String key) { return key.hashCode(); }
+    
+
 
 }
